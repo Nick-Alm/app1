58f2601db325b5e89f1740f61915712b5324a257
Nick-Alm <alm.nicholas.2013@gmail.com>
2017-11-17 20:37:25 +0000

    allowed posting to console from client. Added CSS styling to the submit button

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Shannon' });
+  res.render('index', { title: 'Express' });
+});
+
+/* Post from the text box */
+router.post('/', function(req, res, next) {
+  console.log(req.body.name);
+  res.render('index', { title: req.body.name });
 });
 
 module.exports = router;
